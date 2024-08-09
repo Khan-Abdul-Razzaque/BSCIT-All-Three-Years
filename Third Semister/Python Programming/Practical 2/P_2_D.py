4f2407f069fc7d46f73eac5ef892ea0e7ca661f8
Khan Abdul Razzaque <145262445+Khan-Abdul-Razzaque@users.noreply.github.com>
2024-08-09 14:32:58 +0000

    modified

--- Third Semister/Python Programming/Practical 2/P_2_D.py
+++ Third Semister/Python Programming/Practical 2/P_2_D.py
@@ -1,14 +1,13 @@
 #2-D. Write a program to accept string from the user and check weater it's pamagram or not.
 
 
-def isPangram(string):
+def isPangram(sentence):
     for i in range(26):
-        if chr(i + 65) not in string.upper():
+        if chr(i + 65) not in sentence.upper():
             return False
     return True
-string = input("Enter a string: ")
-funcRes = isPangram(string)
-if funcRes:
-    print("The string is a pangram.")
+sentence = input("Enter a sentence: ")
+if isPangram(sentence):
+    print("The sentence is a pangram.")
 else:
-    print("The string is not a pangram.")
+    print("The sentence is not a pangram.")
